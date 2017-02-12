4c0378ffe1efb5e7ae2f9daa854844ed558fe0b9
pixlark <t1000.pixlark@gmail.com>
2017-02-11 22:02:49 -0500

    First commit.

---
+++ VisualConsole/GameBuffer.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace VisualConsole {
+
+	public class GameBuffer {
+
+		const ConsoleColor DEFAULT_FOREGROUND_COLOR = ConsoleColor.White;
+		const ConsoleColor DEFAULT_BACKGROUND_COLOR = ConsoleColor.Black;
+
+		int WIDTH { get; set; }
+		int HEIGHT { get; set; }
+
+		public ConsoleChar[,] LAST_BUFFER_ARRAY;
+
+		/********************
+		 * PUBLIC FUNCTIONS *
+		 ********************/
+
+		public void WriteCharToPosition(ConsoleChar write_char, Vector2 position) {
+
+			// Error checking
+			if (position.x >= WIDTH)
+				throw new ArgumentOutOfRangeException("x");
+			if (position.y >= HEIGHT)
+				throw new ArgumentOutOfRangeException("y");
+
+			// Write the character to the position
+			Console.SetCursorPosition(position.x, position.y);
+			Console.ForegroundColor = write_char.text_color;
+			Console.BackgroundColor = write_char.bg_color;
+			Console.Write(write_char.character);
+
+			ResetCursor();
+
+		}
+
+		public void WriteScreen(ConsoleChar[,] screen_array) {
+
+			// Rewrites the buffer intelligently, only rewriting things that change
+
+			if (screen_array.GetLength(0) > WIDTH)
+				throw new ArgumentOutOfRangeException("screen_array width (dimension 0)");
+			if (screen_array.GetLength(1) > HEIGHT)
+				throw new ArgumentOutOfRangeException("screen_array height (dimension 1)");
+
+			List<Vector2> changepoints = CheckBufferAgainstLast(screen_array, LAST_BUFFER_ARRAY);
+
+			for (int i = 0; i < changepoints.Count; i++) {
+
+				WriteCharToPosition(screen_array[changepoints[i].x, changepoints[i].y], changepoints[i]);
+
+			}
+
+			CopyBuffer(screen_array, LAST_BUFFER_ARRAY);
+
+		}
+
+		public void CompleteWriteScreen(ConsoleChar[,] screen_array) {
+
+			// Completely rewrites the buffer. Very slow.
+
+			if (screen_array.GetLength(0) > WIDTH)
+				throw new ArgumentOutOfRangeException("screen_array width (dimension 0)");
+			if (screen_array.GetLength(1) > HEIGHT)
+				throw new ArgumentOutOfRangeException("screen_array height (dimension 1)");
+
+			for (int x = 0; x < WIDTH; x++) {
+
+				for (int y = 0; y < HEIGHT; y++) {
+
+					WriteCharToPosition(screen_array[x, y], new Vector2(x, y));
+
+				}
+
+			}
+
+			CopyBuffer(screen_array, LAST_BUFFER_ARRAY);
+
+		}
+
+		/********************
+		 * STATIC FUNCTIONS *
+		 ********************/
+
+		public static void CopyBuffer(ConsoleChar[,] source, ConsoleChar[,] destination) {
+
+			// Thanks to arrays being passed by reference, we need to use this function
+
+			if (source.GetLength(0) != destination.GetLength(0))
+				throw new ArgumentOutOfRangeException("source and destination must have the same lengths");
+			if (source.GetLength(1) != destination.GetLength(1))
+				throw new ArgumentOutOfRangeException("source and destination must have the same lengths");
+
+			for (int x = 0; x < source.GetLength(0); x++) {
+
+				for (int y = 0; y < source.GetLength(1); y++) {
+
+					destination[x, y] = source[x, y];
+
+				}
+
+			}
+
+		}
+
+		public static ConsoleChar[,] LoadScreenFromFile(string file_name, Vector2 size) {
+
+			ConsoleChar[,] screen_array = new ConsoleChar[size.x, size.y];
+
+			using (FileStream file_stream = new FileStream(file_name, FileMode.Open)) {
+
+				// Go through each row rather than each column so that the array is initialized properly
+				for (int y = 0; y < size.y; y++) {
+
+					for (int x = 0; x < size.x; x++) {
+
+						screen_array[x, y] = new ConsoleChar((char)file_stream.ReadByte(), ConsoleColor.Black, ConsoleColor.White);
+
+					}
+
+					if (file_stream.Position == file_stream.Length)
+						break;
+
+					while ((char)file_stream.ReadByte() != '\n') ;
+
+				}
+
+			}
+
+			return screen_array;
+
+		}
+
+		/*********************
+		 * PRIVATE FUNCTIONS *
+		 *********************/
+
+		List<Vector2> CheckBufferAgainstLast(ConsoleChar[,] next_array, ConsoleChar[,] last_array) {
+
+			// Compares two buffers. This is an O(n) operation
+
+			List<Vector2> changepoints = new List<Vector2>();
+
+			for (int x = 0; x < WIDTH; x++) {
+
+				for (int y = 0; y < HEIGHT; y++) {
+
+					if (next_array[x, y] != last_array[x, y]) {
+
+						changepoints.Add(new Vector2(x, y));
+
+					}
+
+
+				}
+
+			}
+
+			return changepoints;
+
+		}
+
+		/*******
+		 * RWs *
+		 *******/
+
+		void ResetCursor() {
+			Console.SetCursorPosition(0, HEIGHT);
+			Console.ForegroundColor = DEFAULT_FOREGROUND_COLOR;
+			Console.BackgroundColor = DEFAULT_BACKGROUND_COLOR;
+		}
+
+		public ConsoleKeyInfo WaitForKey() {
+			ConsoleKeyInfo key = Console.ReadKey();
+			ResetCursor();
+			return key;
+		}
+
+		/**************************
+		 * SETUP AND CONSTRUCTORS * 
+		 **************************/
+
+		public GameBuffer(int width, int height) {
+
+			this.WIDTH = width;
+			this.HEIGHT = height;
+
+			SetupBuffer();
+
+		}
+
+		public GameBuffer(Vector2 size) {
+
+			this.WIDTH = size.x;
+			this.HEIGHT = size.y;
+
+			SetupBuffer();
+
+		}
+
+		void SetupBuffer() {
+
+			// Makes the cursor invisible
+			Console.CursorVisible = false;
+
+			// Sets the actual window height and width
+			Console.WindowWidth = WIDTH;
+			Console.BufferWidth = WIDTH;
+
+			// Set the text buffer to match the window height and width
+			Console.WindowHeight = HEIGHT + 1;
+			Console.BufferHeight = HEIGHT + 1;
+
+			// Puts the cursor in the non-use position
+			Console.SetCursorPosition(0, HEIGHT);
+
+			// Initializes LAST_BUFFER_ARRAY
+			LAST_BUFFER_ARRAY = new ConsoleChar[WIDTH, HEIGHT];
+
+		}
+
+	}
+
+}
---
+++ VisualConsole/Structs.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VisualConsole {
+
+	public struct ConsoleChar {
+
+		public char character;
+		public ConsoleColor text_color;
+		public ConsoleColor bg_color;
+
+		public ConsoleChar(char character, ConsoleColor text_color, ConsoleColor bg_color) {
+
+			this.character = character;
+			this.text_color = text_color;
+			this.bg_color = bg_color;
+
+		}
+
+		public static bool operator ==(ConsoleChar a, ConsoleChar b) {
+
+			return (a.character == b.character && a.text_color == b.text_color && a.bg_color == b.bg_color);
+				
+
+		}
+
+		public static bool operator !=(ConsoleChar a, ConsoleChar b) {
+
+			return !(a.character == b.character && a.text_color == b.text_color && a.bg_color == b.bg_color);
+				
+
+		}
+
+	}
+
+	public struct BufferArray {
+
+		public List<List<ConsoleChar>> array;
+		public int width;
+		public int height;
+
+		public BufferArray(int width, int height) {
+
+			this.array = new List<List<ConsoleChar>>();
+			this.width = width;
+			this.height = height;
+
+		}
+
+	}
+
+	public struct Vector2 {
+
+		public int x;
+		public int y;
+
+		public Vector2(int x, int y) {
+
+			this.x = x;
+			this.y = y;
+
+		}
+
+	}
+
+}
